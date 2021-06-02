a9be667996a17ea1e336c5e96f4e8b14d8b4dabb
alexishlg <alexiparra@gmail.com>
2021-06-02 10:36:01 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,18 @@
+# Modern_Dashboard_Design
+
+Modern Dashboard Design usando CardView 
+
+
+
+![logo](https://i.imgur.com/8mkJZoI.jpg)
+
+Yo use :
+- CardView
+
+
+> Hospital Lucia 2020
+![](Screenshot_1.jpg)
+
+
+
+Alexis Parra – [@cubanito_alex](https://twitter.com/cubanito_alex) – alexiparra@gmail.com
---
+++ app/src/main/java/com/codewithop/modern_dashboard/MainActivity.java
@@ -0,0 +1,17 @@
+package com.codewithop.modern_dashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ScrollView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
